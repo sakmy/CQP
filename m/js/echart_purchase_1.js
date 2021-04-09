c8f9e05504fd5b16a05e9ed747108ce88c91115b
sui540393 <540393741@qq.com>
2021-04-09 18:40:12 +0800

    修改

--- m/js/echart_purchase_1.js
+++ m/js/echart_purchase_1.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   var ch1 = echarts.init(document.getElementById('ch1'));
-  var ch10 = echarts.init(document.getElementById('ch10'));
+  var ch2 = echarts.init(document.getElementById('ch2'));
   var ch4 = echarts.init(document.getElementById('ch4'));
   var ch5 = echarts.init(document.getElementById('ch5'));
-  var ch8 = echarts.init(document.getElementById('ch8'));
+  var ch6 = echarts.init(document.getElementById('ch6'));
 
 
 
@@ -91,7 +91,7 @@ window.onload = function() {
 
 
 
-var op10 = {
+var op2 = {
       tooltip: {
           trigger: 'axis',
           axisPointer: {            // 坐标轴指示器，坐标轴触发有效
@@ -329,7 +329,7 @@ var op5 = {
 
 
 
- var op8 = {
+ var op6 = {
     title: {
       show: false,
       text: '折线图堆叠'
@@ -420,10 +420,10 @@ var op5 = {
 
 
 ch1.setOption(op1);
-ch10.setOption(op10);
+ch2.setOption(op2);
 ch4.setOption(op4);
 ch5.setOption(op5);
-ch8.setOption(op8);
+ch6.setOption(op6);
 
 
 
