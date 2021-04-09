69bc12f79b8a17240558d3becb821acddb489982
sui540393 <540393741@qq.com>
2021-04-09 14:21:02 +0800

    新增+修改

---
+++ m/js/echart_2_5_zx.js
@@ -0,0 +1,487 @@
+window.onload = function() {
+  var ch1 = echarts.init(document.getElementById('ch1'));
+  var ch2 = echarts.init(document.getElementById('ch2'));
+  var ch4 = echarts.init(document.getElementById('ch4'));
+  var ch4_1 = echarts.init(document.getElementById('ch4_1'));
+  var ch5 = echarts.init(document.getElementById('ch5'));
+  var ch5_1 = echarts.init(document.getElementById('ch5_1'));
+  // var ch6 = echarts.init(document.getElementById('ch6'));
+  var op1 = {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+      show:false,
+            type: 'shadow',// 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    grid: {
+        left: '1%',
+        right: '1%',
+        bottom: '0%',
+    top:'10%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            data: ['日用品', '包材类', '其他','其他器具设备','初级农产品','中药饮片','化妆品','保健食品','化学药品','中成药','生物制品','医疗器械','中药材','食品','化学和诊断试剂','（空白）','原辅料类'],
+      axisLabel: {
+         interval:0,
+         rotate:40,
+         margin:20,
+         
+      },
+      
+            axisTick: {
+                alignWithLabel: true
+            },
+      "axisLine":{       //y轴
+          "show":false
+      
+      },
+        }
+    ],
+  
+    yAxis: [
+        {
+            type: 'value',
+      "axisLine":{       //y轴
+        "show":false
+      },
+      splitLine: {
+        lineStyle: {
+          type: 'dashed' // y轴分割线类型
+        }
+      },
+      "axisTick":{       //y轴刻度线
+        "show":false
+      },
+        }
+    ],
+  series: [{
+    type: 'bar',
+    data: [1371, 282,157,112,78,51,41,24,22,21,21,17,13,13,13,12,2],
+    label: {
+      show: true,
+      position: 'top',
+    },
+    barWidth: 34,
+    itemStyle: {
+  
+      normal: {
+       // barBorderRadius:[3, 3, 0, 0],
+        color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [{
+          offset: 0,
+          color: '#1E6EFF'
+        }, {
+          offset: 1,
+          color: '#47E3FF'
+        }]),
+      }
+    },
+  }, ]
+};
+
+  var op2 =  {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+      show:false,
+            type: 'shadow',// 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    grid: {
+        left: '1%',
+        right: '1%',
+        bottom: '0%',
+    top:'10%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            data: ['日用品', '包材类', '其他','其他器具设备','初级农产品','中药饮片','化妆品','保健食品','化学药品','中成药','生物制品','医疗器械','中药材','食品','化学和诊断试剂','（空白）','原辅料类'],
+      axisLabel: {
+         interval:0,
+         rotate:40,
+         margin:20,
+         
+      },
+      
+            axisTick: {
+                alignWithLabel: true
+            },
+      "axisLine":{       //y轴
+          "show":false
+      
+      },
+        }
+    ],
+  
+    yAxis: [
+        {
+            type: 'value',
+      "axisLine":{       //y轴
+        "show":false
+      },
+      splitLine: {
+        lineStyle: {
+          type: 'dashed' // y轴分割线类型
+        }
+      },
+      "axisTick":{       //y轴刻度线
+        "show":false
+      },
+        }
+    ],
+  series: [{
+    type: 'bar',
+    data: [1371, 282,157,112,78,51,41,24,22,21,21,17,13,13,13,12,2],
+    label: {
+      show: true,
+      position: 'top',
+    },
+    barWidth: 34,
+    itemStyle: {
+  
+      normal: {
+       // barBorderRadius:[3, 3, 0, 0],
+        color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [{
+          offset: 0,
+          color: '#FD5900'
+        }, {
+          offset: 1,
+          color: '#FFDE00'
+        }]),
+      }
+    },
+  }, ]
+};
+  
+ 
+  var op4 = {
+    title: {
+      show: false,
+      text: '折线图堆叠'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      show: false,
+      data: ['同期期末应付账款', '期末应付账款', '期末应付账款同比增长率']
+    },
+    grid: {
+      left: '5%',
+      top: '8%',
+      right: '4%',
+      bottom: '15%',
+      containLabel: false
+    },
+    toolbox: {
+      show: false,
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: true,
+      axisTick: {
+        alignWithLabel: true
+      },
+      data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+    },
+    yAxis: {
+      axisTick: {
+        show: false
+      },
+    },
+    series: [{
+      name: '同期期末应付账款',
+      type: 'bar',
+      barWidth: '30%',
+      itemStyle: {
+        normal: {
+          barBorderRadius: [3, 3, 0, 0],
+          color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [{
+            offset: 0,
+            color: '#47E3FF'
+          }, {
+            offset: 1,
+            color: '#1E6EFF'
+          }]),
+        }
+      },
+      data: [140, 132, 101, 134, 90, 230, 210, 101, 134, 90, 230, 210]
+    }, {
+      name: '期末应付账款',
+      type: 'bar',
+      barWidth: '30%',
+      itemStyle: {
+        normal: {
+          barBorderRadius: [3, 3, 0, 0],
+          color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [{
+            offset: 0,
+            color: '#71EEB7'
+          }, {
+            offset: 1,
+            color: '#00BEC5'
+          }]),
+        }
+      },
+      data: [240, 232, 101, 134, 90, 230, 210, 101, 134, 90, 230, 210]
+    }, {
+      name: '期末应付账款同比增长率',
+      type: 'line',
+      //symbol: 'circle',
+      symbolSize: 10,
+      smooth: true,
+      color: '#0064DC',
+      data: [50, 42, 11, 44, 100, 240, 220, 111, 144, 100, 240, 220]
+    }]
+  };
+  var op4_1 = {
+    title: {
+      show: false,
+      text: '折线图堆叠'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      show: false,
+      data: ['同期期末应付账款', '期末应付账款', '期末应付账款同比增长率']
+    },
+    grid: {
+      left: '5%',
+      top: '8%',
+      right: '4%',
+      bottom: '15%',
+      containLabel: false
+    },
+    toolbox: {
+      show: false,
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: true,
+      axisTick: {
+        alignWithLabel: true
+      },
+      data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+    },
+    yAxis: {
+      axisTick: {
+        show: false
+      },
+    },
+    series: [{
+      name: '同期期末应付账款',
+      type: 'bar',
+      barWidth: '30%',
+      itemStyle: {
+        normal: {
+          barBorderRadius: [3, 3, 0, 0],
+          color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [{
+            offset: 0,
+            color: '#47E3FF'
+          }, {
+            offset: 1,
+            color: '#1E6EFF'
+          }]),
+        }
+      },
+      data: [140, 132, 101, 134, 90, 230, 210, 101, 134, 90, 230, 210]
+    }, {
+      name: '期末应付账款',
+      type: 'bar',
+      barWidth: '30%',
+      itemStyle: {
+        normal: {
+          barBorderRadius: [3, 3, 0, 0],
+          color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [{
+            offset: 0,
+            color: '#71EEB7'
+          }, {
+            offset: 1,
+            color: '#00BEC5'
+          }]),
+        }
+      },
+      data: [240, 232, 101, 134, 90, 230, 210, 101, 134, 90, 230, 210]
+    }, {
+      name: '期末应付账款同比增长率',
+      type: 'line',
+      //symbol: 'circle',
+      symbolSize: 10,
+      smooth: true,
+      color: '#0064DC',
+      data: [50, 42, 11, 44, 100, 240, 220, 111, 144, 100, 240, 220]
+    }]
+  };
+  var op5 =  {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+      show:false,
+            type: 'shadow',// 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    grid: {
+        left: '1%',
+        right: '1%',
+        bottom: '0%',
+    top:'10%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            data: ['日用品', '包材类', '其他','其他器具设备','初级农产品','中药饮片','化妆品','保健食品','化学药品','中成药','生物制品','医疗器械','中药材','食品','化学和诊断试剂','（空白）','原辅料类'],
+      axisLabel: {
+         interval:0,
+         rotate:40,
+         margin:20,
+         
+      },
+      
+            axisTick: {
+                alignWithLabel: true
+            },
+      "axisLine":{       //y轴
+          "show":false
+      
+      },
+        }
+    ],
+  
+    yAxis: [
+        {
+            type: 'value',
+      "axisLine":{       //y轴
+        "show":false
+      },
+      splitLine: {
+        lineStyle: {
+          type: 'dashed' // y轴分割线类型
+        }
+      },
+      "axisTick":{       //y轴刻度线
+        "show":false
+      },
+        }
+    ],
+  series: [{
+    type: 'bar',
+    data: [1371, 282,157,112,78,51,41,24,22,21,21,17,13,13,13,12,2],
+    label: {
+      show: true,
+      position: 'top',
+    },
+    barWidth: 34,
+    itemStyle: {
+  
+      normal: {
+       // barBorderRadius:[3, 3, 0, 0],
+        color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [{
+          offset: 0,
+          color: '#1E6EFF'
+        }, {
+          offset: 1,
+          color: '#47E3FF'
+        }]),
+      }
+    },
+  }, ]
+};
+
+  
+  var op5_1 =  {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+      show:false,
+            type: 'shadow',// 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    grid: {
+        left: '1%',
+        right: '1%',
+        bottom: '0%',
+    top:'10%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            data: ['日用品', '包材类', '其他','其他器具设备','初级农产品','中药饮片','化妆品','保健食品','化学药品','中成药','生物制品','医疗器械','中药材','食品','化学和诊断试剂','（空白）','原辅料类'],
+      axisLabel: {
+         interval:0,
+         rotate:40,
+         margin:20,
+         
+      },
+      
+            axisTick: {
+                alignWithLabel: true
+            },
+      "axisLine":{       //y轴
+          "show":false
+      
+      },
+        }
+    ],
+  
+    yAxis: [
+        {
+            type: 'value',
+      "axisLine":{       //y轴
+        "show":false
+      },
+      splitLine: {
+        lineStyle: {
+          type: 'dashed' // y轴分割线类型
+        }
+      },
+      "axisTick":{       //y轴刻度线
+        "show":false
+      },
+        }
+    ],
+  series: [{
+    type: 'bar',
+    data: [1371, 282,157,112,78,51,41,24,22,21,21,17,13,13,13,12,2],
+    label: {
+      show: true,
+      position: 'top',
+    },
+    barWidth: 34,
+    itemStyle: {
+  
+      normal: {
+       // barBorderRadius:[3, 3, 0, 0],
+        color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [{
+          offset: 0,
+          color: '#1E6EFF'
+        }, {
+          offset: 1,
+          color: '#47E3FF'
+        }]),
+      }
+    },
+  }, ]
+};
+
+  
+  ch1.setOption(op1);
+  ch2.setOption(op2);
+  ch4.setOption(op4);
+  ch4_1.setOption(op4_1);
+  ch5.setOption(op5);
+  ch5_1.setOption(op5_1);
+  // ch6.setOption(op6);
+}
